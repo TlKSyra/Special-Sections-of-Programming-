794b3668c3e41fc6c2d8cba13fdb3a956bec2479
TlKSyra <terekhov.ks@gmail.com>
2022-12-02 19:15:36 +0300

    Правка кода

--- Terekhov_K_S_PMI_Lab_6/main.py
+++ Terekhov_K_S_PMI_Lab_6/main.py
@@ -2,15 +2,12 @@ import random
 
 from sortedcontainers import SortedSet
 from collections.abc import Hashable
-
 print('''1. Создаем множество a_set, содержащее не менее 7 элементов любых разрешенных типов, с помощью 3 – генератора.''')
-
 a_set = {x**2 for x in range(7)}
 print(f'Элементы множества a_set: {a_set}')
 
 print('''2. Создает итерабельный объект it_ob, содержащий не менее трех элементов, имеющихся в объекте a_set, и проверить, 
 все ли элементы it_ob хэшируемы. Если нет – заменить нехэшируемые элементы хэшируемыми.''')
-
 it_ob = ['d', 'e', [9.5, 17.3], {'abc', 'dbe'}]
 # сначала добавляю три элемента из множества
 a_set_sort = SortedSet(a_set)
@@ -27,7 +24,6 @@ for index, item in enumerate(it_ob):
 print(it_ob)
 
 print('''3. Преобразует объект it_ob в множество b_set и выполняет над множествами a_set и b_set операцию 3 – difference().''')
-
 b_set = set(it_ob)
 print(f'Элементы нового множества b_set {b_set}')
 print(f'Множество b_set не содержит {a_set.difference(b_set)} \n')
@@ -40,7 +36,6 @@ a_dict = {x: y for x in 'ABC' for y in 'XYZ'}
 print(f'Создан словарь: {a_dict}')
 
 print('''5. Выполняет следующие методы словаря a_dict:1 – clear(), 3 – items(), 5 – pop(key[, default)''')
-
 print(f'Результат выполнения метода items(): {a_dict.items()}')
 # для выполнения следующего метода выбирался рандомный элемент словаря
 print(f'Результат выполнения метода pop(): {a_dict.pop(random.choice(list(a_dict)))}')
